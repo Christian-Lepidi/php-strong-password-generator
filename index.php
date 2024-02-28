c0e857134e586d655ac767d3d32ec651be00d4f1
Christian-Lepidi <christianlepidi@gmail.com>
2024-02-28 17:05:06 +0100

    fix: change four arrays in one string

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-$lowercase_letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-$uppercase_letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-$numbers =["1","2","3","4","5","6","7","8","9"];
-$symbols =["<",">","!","£","$","%","&","/","(",")","=","?","*","#","@"];
+$possible_characters ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789<>!£$%&/()=?*#@";
+
 $form_sent = !empty($_GET["user-number"]);
 
 if ($form_sent){
