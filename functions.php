a7c199b3fedbd6ec6c56519b4be5d24aa44801b0
Christian-Lepidi <christianlepidi@gmail.com>
2024-02-29 10:26:14 +0100

    fix: correct symbols for password

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function randomPassword($user_number){
     $password ="";
-    $possible_characters ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£%&().,";
+    $possible_characters ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!%&().,";
     
     for ($i=0; $i < $user_number; $i++){
        $n= rand(0, strlen($possible_characters)-2);
